8f3f4cdb33c64a0f9c43846fcc263aaf2d0d0f09
youssr <cyoussr@gmail.com>
2025-12-05 02:52:01 +0100

    401 error fix

--- src/components/PilotsMap.jsx
+++ src/components/PilotsMap.jsx
@@ -27,9 +27,9 @@ export default function PilotsMap() {
         style={{ width: "100%", height: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+/>
 
         {pilotLocations.map((location, index) => (
           <Marker key={index} position={location.coords}>
